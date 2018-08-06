135f7800342a9320a980bfddd90b7b8bf98084b3
Krishanu Mallik Thakur <mkrishanu98@gmail.com>
2018-08-06 21:09:40 +0530

    final

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ if('serviceWorker' in navigator){
     // console.log('Service worker supported!');
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('../sw_cached_pages.js')
+            .register('../sw_cached_site.js')
             .then(reg => console.log('Service Worker registered'))
             .catch(err => console.log(`Error: ${err}`));
     });
